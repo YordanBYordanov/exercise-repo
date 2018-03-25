675341be7b6dbb6d2fcc25ba95179cde4e6289e0
YordanBYordanov <dancho.86@abv.bg>
2018-03-25 21:41:41 +0300

    AddCode

---
+++ codeFile.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            double[] masVal = new double[numbers];
+            double maxVal = double.MinValue;
+            for (int i = 0; i < numbers; i++)
+            {
+                double valIn = double.Parse(Console.ReadLine());
+                if(valIn > maxVal)
+                {
+                    maxVal = valIn;
+                }
+                masVal[i] = valIn;
+            }
+
+            bool firstCheck = false;
+            for (int i = 0; i < numbers; i++)
+            {
+                if(masVal[i] == maxVal && firstCheck == false)
+                {
+                    firstCheck = true;
+                }
+                else
+                {
+                    sum += masVal[i];
+                }
+               
+            }
+            if(sum == maxVal)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxVal}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxVal - sum)}");
+            }
+        }
+    }
+}
